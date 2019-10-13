0aab4d224646c76752b375372c84df59c5ee59c0
WuHao <wuhao9714@gmail.com>
2019-10-13 14:48:54 +0800

    修改 SimpleThread

--- code/threads/threadtest.cc
+++ code/threads/threadtest.cc
@@ -33,7 +33,7 @@ SimpleThread(int which)
     
     for (num = 0; num < which; num++) {
 	// printf("*** thread %d looped %d times\n", which, num);
-        printf("*** thread name %s userid %d threadid %d looped %d times \n",
+        printf("*** thread name:%s userid:%d threadid:%d looped:%d times \n",
             currentThread->getName(),currentThread->getUserID(),currentThread->getThreadID(),num);
         currentThread->Yield();
     }
