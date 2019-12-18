f2e1c3842bbfae88f7b93e6cafe974ef13ad8ae8
WuHao <wuhao9714@gmail.com>
2019-12-18 20:20:22 +0800

    scheduler.run-switch

--- code/machine/mipssim.cc
+++ code/machine/mipssim.cc
@@ -35,8 +35,6 @@ Machine::Run()
     if(DebugIsEnabled('m'))
         printf("Starting thread \"%s\" at time %d\n",
 	       currentThread->getName(), stats->totalTicks);
-    printf("Starting thread \"%s\" at time %d\n",
-	       currentThread->getName(), stats->totalTicks);
     interrupt->setStatus(UserMode);
     for (;;) {
         OneInstruction(instr);
