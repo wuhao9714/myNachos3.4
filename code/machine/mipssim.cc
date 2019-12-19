8e43f09844d9bf974e12b4319b330830de226593
WuHao <wuhao9714@gmail.com>
2019-12-19 12:15:37 +0800

    nothing

--- code/machine/mipssim.cc
+++ code/machine/mipssim.cc
@@ -37,6 +37,7 @@ Machine::Run()
 	       currentThread->getName(), stats->totalTicks);
     interrupt->setStatus(UserMode);
     for (;;) {
+    	printf("instr\n");
         OneInstruction(instr);
 	interrupt->OneTick();
 	if (singleStep && (runUntilTime <= stats->totalTicks))
